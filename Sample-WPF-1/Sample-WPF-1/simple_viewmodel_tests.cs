de704742a5caafdc43908eead5c2b324dad944d7
Andrey Tsygankov <tsygankov@gmail.com>
2018-07-19 16:42:08 -0400

    - added test with IsOrBecomeTrue()

--- Sample-WPF-1/Sample-WPF-1/simple_viewmodel_tests.cs
+++ Sample-WPF-1/Sample-WPF-1/simple_viewmodel_tests.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Microsoft.Reactive.Testing;
+using ReactiveDomain.Testing;
 using ReactiveUI;
+using ReactiveUI.Testing;
+using System;
+using System.Reactive.Concurrency;
+using System.Threading;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Sample_WPF_1
 {
-    public class simple_viewmodel_tests
+    public class simple_viewmodel_tests : ReactiveTest
     {
         private readonly ITestOutputHelper _toh;
         public simple_viewmodel_tests(ITestOutputHelper toh)
         {
             _toh = toh;
+            RxApp.MainThreadScheduler = Scheduler.CurrentThread;
         }
 
 
@@ -38,5 +39,54 @@ namespace Sample_WPF_1
             
             _toh.WriteLine("Test completed");
         }
+
+        [Fact]
+        public void can_update_can_execute_with_RD()
+        {
+            var vm = new ViewModelClass();
+
+            using (var notifications = vm.AddName.CanExecute.CreateCollection())
+            {
+
+                vm.FirstName = "hgdghfgsdfhjg";
+                AssertEx.IsOrBecomesTrue(() => notifications.Count > 1);
+                Assert.False(notifications[0]);
+                Assert.True(notifications[1]);
+            }
+                
+        }
+
+
+        //[Fact]
+        //public void can_update_can_execute()
+        //{
+        //    new TestScheduler().With(scheduler =>
+        //    {
+        //        var vm = new ViewModelClass();
+
+        //        scheduler.Schedule(TimeSpan.FromMilliseconds(100), () => { vm.FirstName = "AAA";
+        //            vm.FirstName = ""; 
+        //        });
+
+        //        var results = scheduler.Start(
+        //            () => vm.WhenAnyValue(x => x.AddName.CanExecute));
+
+        //        //var expected = scheduler.CreateColdObservable(
+        //        //    OnNext(100, true),
+        //        //    OnNext(200, false));
+        //        var expected = new[]
+        //        {
+        //            OnNext(100, true),
+        //            OnNext(200, false)
+        //        };
+
+        //        //ReactiveAssert.AreElementsEqual<bool>(expected, results.Messages);
+        //        //results.Messages.AssertEqual(expected.Messages);
+        //        //IEnumerable<object> expected = new[] { OnNext(20, new Unit()) };
+        //        //ReactiveAssert.AreElementsEqual(expected.Messages, results.Messages);
+        //    });
+
+        //}
+
     }
 }
