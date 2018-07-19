a6a71e229e24f4f7512dcc19404a773fa348cb1b
Andrey Tsygankov <tsygankov@gmail.com>
2018-07-19 13:30:05 -0400

    added PetNameView and PersonNameView

---
+++ Sample-WPF-1/Sample-WPF-1/PersonNameView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ReactiveUI;
+
+namespace Sample_WPF_1
+{
+    /// <summary>
+    /// Interaction logic for PersonNameView.xaml
+    /// </summary>
+    public partial class PersonNameView : IViewFor<PersonName>
+    {
+        public PersonNameView()
+        {
+            InitializeComponent();
+        }
+
+        #region ViewModel property boilerplate
+
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (PersonName)value; }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register("ViewModel", typeof(PersonName), typeof(PersonNameView));
+
+        public PersonName ViewModel
+        {
+            get { return (PersonName)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        #endregion
+    }
+}
---
+++ Sample-WPF-1/Sample-WPF-1/PetNameView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ReactiveUI;
+
+namespace Sample_WPF_1
+{
+    /// <summary>
+    /// Interaction logic for PetNameView.xaml
+    /// </summary>
+    public partial class PetNameView : IViewFor<PetName>
+    {
+        public PetNameView()
+        {
+            InitializeComponent();
+        }
+
+        #region ViewModel property boilerplate
+
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (PetName)value; }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register("ViewModel", typeof(PetName), typeof(PetNameView));
+
+        public PetName ViewModel
+        {
+            get { return (PetName)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        #endregion
+    }
+}
